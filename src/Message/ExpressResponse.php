2d82763d8120538e9814b63e1469bb7ce99a2ac7
wjminions <sz_022@outlook.com>
2017-07-31 13:00:23 +0800

    1.0.8

--- src/Message/ExpressResponse.php
+++ src/Message/ExpressResponse.php
@@ -53,6 +53,7 @@ class ExpressResponse extends AbstractResponse
      */
     public function isSuccessful()
     {
+        // 訂單請求過銀聯后必須用origRespCode來判斷支付是否成功
         if (isset($this->data['origRespCode'])) {
             if ($this->data['origRespCode'] === '00') {
                 return true;
@@ -61,11 +62,13 @@ class ExpressResponse extends AbstractResponse
             }
         }
 
+        // 訂單沒有請求過銀聯用respCode來判斷支付是否成功
         return isset($this->data['respCode']) && $this->data['respCode'] === '00';
     }
 
     public function getMessage()
     {
+        // 訂單請求過銀聯后必須用origRespMsg獲取支付信息
         return isset($this->data['origRespMsg']) ? $this->data['origRespMsg'] : $this->data['respMsg'];
     }
 }
